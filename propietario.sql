ba92d06108c8c91054d4e76e29ef3d28bd00b343
BryanL22 <90713107+BryanL22@users.noreply.github.com>
2026-04-15 18:23:13 -0500

    Delete propietario.sql

--- propietario.sql
+++
@@ -1,5 +0,0 @@
-/*
--- Query: SHOW CREATE TABLE propietario
--- Date: 2026-04-15 18:19
-*/
-INSERT INTO `` (`Table`,`Create Table`) VALUES ('propietario','CREATE TABLE `propietario` (\n  `id` varchar(20) NOT NULL,\n  `nombre` varchar(100) DEFAULT NULL,\n  PRIMARY KEY (`id`)\n) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci');
